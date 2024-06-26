25e994a576cdfbeefe1ff629d8249a26b1680a66
shifachaus <chausshifa804@gmail.com>
2024-06-26 14:33:12 +0530

    style homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import heroimage from "../assets/heroimage.png";
 
 const Homepage = () => {
-  return <div>Travel world</div>;
+  return (
+    <main
+      style={{
+        "--image-url": `url(${heroimage})`,
+        backgroundImage: `linear-gradient(rgba(36,42,46,0.8), rgba(36,42,46,0.8)), url(${heroimage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="  mx-auto py-4 px-8  h-screen  "
+    >
+      <Link to={"/"}>
+        <h1 className="text-3xl font-black mb-6 text-neutral-50">
+          Travel world
+        </h1>
+      </Link>
+      <section className="relative flex flex-col items-start md:items-center  pt-24  lg:pt-48 max-w-4xl mx-auto">
+        <h2 className="text-3xl mb-6 text-neutral-200 font-semibold ">
+          You travel the world. WorldWise keeps track of your adventures.
+        </h2>
+
+        <p className="text-sm mb-8  text-neutral-300 max-w-xl">
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </p>
+
+        <Link
+          to={"login"}
+          className="bg-primary  border-primary hover:border-neutral-400  text-neutral-200 rounded-md py-3 px-6 border text-sm  cursor-pointer"
+        >
+          START TRACKING NOW
+        </Link>
+      </section>
+    </main>
+  );
 };
 
 export default Homepage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+import heroimage from "./src/assets/heroimage.png";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to right bottom, rgba(49, 84, 44, 0.8), rgba(16, 71, 52, 0.8)), url(heroimage)",
+      },
+
+      colors: {
+        primary: "#167351",
+      },
+    },
   },
   plugins: [],
 };
