fc5bb0f0bb3591830e008f194822acf190261f54
shifachaus <chausshifa804@gmail.com>
2024-06-27 20:43:15 +0530

    Highlight selected city

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,14 +9,19 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 const CityItem = ({ city }) => {
+  const { currentCity } = useCities();
+
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
       <Link
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-        className="text-zinc-300 font cursor-pointer  py-4 px-3 rounded-md flex justify-between gap-4 mb-2 
-	border border-l-4 border-l-primary border-t-[#2C3747] border-b-[#2C3747] border-r-[#2C3747] hover:bg-[#2C3747]"
+        className={` ${
+          id === currentCity.id
+            ? "border border-l-4 border-l-primary border-primary"
+            : "border border-l-4 border-l-primary border-t-[#2C3747] border-b-[#2C3747] border-r-[#2C3747] "
+        } text-zinc-300 font cursor-pointer py-4 px-3 rounded-md flex justify-between gap-4 mb-2 hover:bg-[#2C3747]`}
       >
         <div className="flex gap-4 items-center">
           <span className="text-md">{emoji}</span>
