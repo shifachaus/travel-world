b43ab18925f2dd229899da7dcabae5180e295f22
shifachaus <chausshifa804@gmail.com>
2024-06-27 21:30:26 +0530

    display city markers on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,33 +1,42 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 const Map = () => {
   const navigate = useNavigate();
-  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
+  console.log(cities);
   return (
-    <div className="mx-auto  px-8 h-screen " onClick={() => navigate("form")}>
+    <div className="mx-auto h-screen " onClick={() => navigate("form")}>
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className="h-[100%]"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -7,15 +7,19 @@ const AppLayout = () => {
 
   return (
     <div className="relative">
-      <p
-        onClick={() => setShow(!show)}
-        className={`absolute z-[10] text-3xl ${
-          show ? "text-zinc-100" : "text-black"
-        } top-2 left-3 cursor-pointer`}
-      >
-        {">"}
-      </p>
-      {show && <Sidebar />}
+      <div className="absolute z-[9999] top-0">
+        <>
+          <p
+            onClick={() => setShow(!show)}
+            className={`absolute z-[10]  text-3xl ${
+              show ? "text-zinc-100" : "text-black"
+            } top-20 left-3 cursor-pointer`}
+          >
+            {">"}
+          </p>
+          {show && <Sidebar />}
+        </>
+      </div>
       <Map />
     </div>
   );
